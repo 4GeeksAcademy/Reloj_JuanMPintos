4a4815d037abd1c2ba45459a5e30fd630de1d4b6
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-02 11:24:00 +0000

    falta css

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "/workspaces/Reloj_JuanMPintos/src/styles/index.css"
+
+function ContadorDeSegundos(prop){
+    return (<h1><i className="fa-regular fa-clock"></i>{prop.contador}</h1>)
+}
+
+export default ContadorDeSegundos
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,16 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// include your styles into the webpack bundle
+import ContadorDeSegundos from "../js/component/SecondsCounter.jsx";
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
+let contador = 0
+
+contador = setInterval(() => {
+contador++    
+
+ReactDOM.createRoot(document.getElementById('app')).render(<ContadorDeSegundos contador={contador} />);
+} ,1000)
 
